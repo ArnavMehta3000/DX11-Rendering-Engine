8257c4632b3e4d3567edc201e3491be32eaed826
Arnav Mehta <arnavdatt@gmail.com>
2021-11-15 23:40:46 +0000

    Revert "Added Engine class"
    This reverts commit dafd7a66cb24c544cea49f84b3f22078f022c5a7.

--- DX11 Framework/DX11 Framework.cpp
+++ DX11 Framework/DX11 Framework.cpp
@@ -1,24 +1,21 @@
 #include "Application.h"
-#include"Engine.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	/*Application * theApp = new Application();
+	Application * theApp = new Application();
 
 	if (FAILED(theApp->Initialise(hInstance, nCmdShow)))
 	{
 		return -1;
-	}*/
-
-	Engine::GetInstance(hInstance, nCmdShow);
+	}
 
 	// Main message loop
 	MSG msg = {0};
 
-	while (WM_QUIT != msg.message && Engine::IsRunning())
+	while (WM_QUIT != msg.message)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
@@ -27,17 +24,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		}
 		else
 		{
-			/*theApp->Update();
-			theApp->Draw();*/
-			Engine::GetInstance()->Update();
-			Engine::GetInstance()->Render();
+			theApp->Update();
+			theApp->Draw();
 		}
 	}
 
-	Engine::GetInstance()->Clean();
-
-	/*delete theApp;
-	theApp = nullptr;*/
+	delete theApp;
+	theApp = nullptr;
 
 	return (int) msg.wParam;
 }
